9774720437ab4b93a3d3f8d001a7d564d3dd415f
1w6qn <2050903851@qq.com>
2024-07-09 09:20:40 +0800

    add hook

---
+++ hook/main.ts
@@ -0,0 +1,32 @@
+import 'frida-il2cpp-bridge';
+const serverUrl = "192.168.0.6:8443"
+let log = console.log
+Java.perform(() => {
+    const sdk = Java.use("com.hypergryph.platform.hgsdk.contants.SDKConst$UrlInfo")
+    sdk.getRemoteUrl.implementation = function () {
+        log("[Java Layer]Changed Hypergryph SDK")
+        return `http://${serverUrl}/auth`
+    }
+    const sdk2 = Java.use("com.hypergryph.platform.hguseragreement.contans.SDKConst$UrlInfo")
+    sdk2.getRemoteUrl.implementation = function () {
+        log("[Java Layer]Changed Hypergryph user agreement")
+        return `http://${serverUrl}/auth`
+    }
+})
+
+setTimeout(() => Il2Cpp.perform(() => {
+    //Il2Cpp.dump("d.cs")
+    const Networker = Il2Cpp.domain.assembly("Torappu.Common").image.class("Torappu.Network.Networker");
+    const GetOverrideRouterUrl = Networker.method<Il2Cpp.String>("get_overrideRouterUrl");
+    GetOverrideRouterUrl.implementation = function (): Il2Cpp.String {
+        return Il2Cpp.string(`http://${serverUrl}/config/prod/official/network_config`);
+    }
+    log("[Il2Cpp Layer]Hooked GetOverrideRouterUrl")
+    const CryptUtils = Il2Cpp.domain.assembly("Assembly-CSharp").image.class("Torappu.CryptUtils");
+    const VerifySignMD5RSA = CryptUtils.method<boolean>("VerifySignMD5RSA");
+    //@ts-ignore
+    VerifySignMD5RSA.implementation = function (a: Il2Cpp.String, b: Il2Cpp.String, c: Il2Cpp.String): boolean {
+        log("[Il2Cpp Layer]Hooked VerifySignMD5RSA")
+        return true
+    };
+}), 5000);

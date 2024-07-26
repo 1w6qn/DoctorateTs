1dc8db72c44ab0ccee90afc4d7a1d2c12cafb993
1w6qn <2050903851@qq.com>
2024-07-26 17:21:44 +0800

    update

---
+++ app/game/controller/MailController.ts
@@ -0,0 +1,20 @@
+export class MailController {
+    uid:string
+    constructor(uid:string) {
+        this.uid = uid;
+    }
+    listMailbox(args:{sysMailIdList:number[], surveyMailIdList:string[], mailIdList:number[]}){
+
+    }
+    receiveMail(){
+
+    }
+    getMetaInfoList(){
+
+    }
+    receiveAllMail(){}
+    removeAllReceivedMail(){}
+}
+export class MailGet{
+    
+}
